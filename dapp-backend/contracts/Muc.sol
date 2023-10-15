12d1053cda8b0992308544a6bce7dfe07cd7c033
OmarMesqq <omarmsqt@gmail.com>
2023-10-15 14:14:00 -0300

    refactor: :heavy_minus_sign: remove counters dep as its deprecated

--- dapp-backend/contracts/Muc.sol
+++ dapp-backend/contracts/Muc.sol
@@ -2,28 +2,26 @@
 pragma solidity ^0.8.1;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 // Inherits from the ERC-721 standard 
 contract Muccoin is ERC721URIStorage {
-    using Counters for Counters.Counter;
-    Counters.Counter private _tokenIds;
+    uint256 private _tokenIds;
 
     // Called once the contract is deployed
     constructor() ERC721("Muccoin", "MUC") {}
 
     // * @param recipient: address (20 bytes) of the recipient of the token
     // * @param tokenURI: is stored only during the execution of the function in temporary storage
-    function mintNFT(address recipient, string memory tokenURI)
+    function mintToken(address recipient, string memory tokenURI)
         public
         returns (uint256)
     {
-        _tokenIds.increment(); // ensures that each token has a unique ID
+        _tokenIds++;  // ensures that each token has a unique ID
 
-        uint256 newItemId = _tokenIds.current();
-        _mint(recipient, newItemId); // creates a new token and assigns its ownership
-        _setTokenURI(newItemId, tokenURI); // associates new token with its original metadata
+        uint256 newItemId = _tokenIds;
+        _mint(recipient, newItemId);  // creates a new token and assigns its ownership
+        _setTokenURI(newItemId, tokenURI);  // associates new token with its original metadata
 
         return newItemId;
     }
