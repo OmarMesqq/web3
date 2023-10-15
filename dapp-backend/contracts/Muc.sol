d27ee4cfe88fb238b700eccbf0c0e30428023285
OmarMesqq <omarmsqt@gmail.com>
2023-10-15 15:34:16 -0300

    refactor: :recycle: emit event instead of returning value for a more idiomatic approach

--- dapp-backend/contracts/Muc.sol
+++ dapp-backend/contracts/Muc.sol
@@ -4,25 +4,23 @@ pragma solidity ^0.8.1;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-// Inherits from the ERC-721 standard 
+// Inherits from the ERC-721 standard
 contract Muccoin is ERC721URIStorage {
     uint256 private _tokenIds;
 
     // Called once the contract is deployed
     constructor() ERC721("Muccoin", "MUC") {}
 
-    // * @param recipient: address (20 bytes) of the recipient of the token
-    // * @param tokenURI: is stored only during the execution of the function in temporary storage
-    function mintToken(address recipient, string memory tokenURI)
-        public
-        returns (uint256)
-    {
-        _tokenIds++;  // ensures that each token has a unique ID
+    event TokenMinted(uint256 tokenId);
 
+    function mintToken(address recipient, string memory tokenURI) public {
+        _tokenIds++;
         uint256 newItemId = _tokenIds;
-        _mint(recipient, newItemId);  // creates a new token and assigns its ownership
-        _setTokenURI(newItemId, tokenURI);  // associates new token with its original metadata
+        _mint(recipient, newItemId); // creates a new token and assigns its ownership
+        _setTokenURI(newItemId, tokenURI); // associates new token with its original metadata
 
-        return newItemId;
+        emit TokenMinted(newItemId);
+        // In ethers.js, contract method calls return a transaction response,
+        // not the return value of the function
     }
 }
